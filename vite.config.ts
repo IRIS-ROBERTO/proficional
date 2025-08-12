b96e03336456da4b555555e80338ab16861e0d59
IRIS ROBERTO DOS SANTOS FERREIRA ROBERTO <iris369.iart@gmail.com>
2025-08-11 22:34:06 -0300

    Corrigir base path para GitHub Pages (proficional)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/portefolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/proficional/' : '/',
   server: {
     host: "0.0.0.0",
     port: 3030,
